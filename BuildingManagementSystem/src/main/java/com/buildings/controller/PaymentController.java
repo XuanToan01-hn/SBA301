32e2fd251a281b1e2b70ebb1a21c9ee63cf16b7b
tungtq123 <he182488toquoctung@gmail.com>
2026-02-25 10:16:15 +0700

    config cors for payment controller

--- BuildingManagementSystem/src/main/java/com/buildings/controller/PaymentController.java
+++ BuildingManagementSystem/src/main/java/com/buildings/controller/PaymentController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@CrossOrigin("http://localhost:5173")
 @RestController
 @RequestMapping("/api/payment")
 @RequiredArgsConstructor
