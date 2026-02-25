63c2388a81d9814e4e94a5f9e72da4643cca62b0
tungtq123 <he182488toquoctung@gmail.com>
2026-02-25 12:37:06 +0700

    cá»Ãors config for payment controller

--- src/main/java/com/buildings/controller/PaymentController.java
+++ src/main/java/com/buildings/controller/PaymentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/payment")
+@RequestMapping("/api/v1/payment")
 @RequiredArgsConstructor
 public class PaymentController {
     private final PaymentService paymentService;
